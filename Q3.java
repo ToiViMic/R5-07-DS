f88a1f152d76dfbbe1274fc6fc0082be4e2bf073
Antoine Caron <103735534+ToiViMic@users.noreply.github.com>
2024-11-20 14:14:17 +0100

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,17 @@
+public class Q3 {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Veuillez fournir le nombre d'étoiles en paramètre.");
+            return;
+        }
+        try {
+            int n = Integer.parseInt(args[0]);
+            for (int i = 0; i < n; i++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        } catch (NumberFormatException e) {
+            System.out.println("Veuillez entrer un nombre valide !");
+        }
+    }
+}
