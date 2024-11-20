4aec529c39de23397c84d45ae09ac2020bd2768e
Antoine Caron <103735534+ToiViMic@users.noreply.github.com>
2024-11-20 14:50:00 +0100

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,20 @@
+public class Q6 {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Veuillez fournir le nombre d'étoiles en paramètre.");
+            return;
+        }
+        try {
+            int n = Integer.parseInt(args[0]);
+            // Affichage inversé du triangle
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < n - i + 1; j++) {
+                    System.out.print("x");//affichage du caractère x 
+                }
+                System.out.println();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Veuillez entrer un nombre valide.");
+        }
+    }
+}
